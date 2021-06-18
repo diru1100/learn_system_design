432bab64cc16c7800a4eae6b1815c6b5344a0ab5
diru1100 <rohitdandamudi.1100@gmail.com>
2021-06-18 13:27:05 +0530

    Add singleton example

--- python_design_patterns/Exercise Files/Ch02/02_06/singleton.py
+++ python_design_patterns/Exercise Files/Ch02/02_06/singleton.py
@@ -1,12 +1,28 @@
 class Borg:
-    
+
     """The Borg design pattern"""
-    
+    _shared_state = {}
 
-        
+    def __init__(self) -> None:
+        self.__dict__ = self._shared_state
 
-class Singleton(Borg): 
-    
+
+class Singleton(Borg):
+
     """The singleton class"""
-   
 
+    def __init__(self, **kwargs) -> None:
+        Borg.__init__(self)
+        self._shared_state.update(kwargs)
+
+    def __str__(self):
+        return str(self._shared_state)
+
+
+x = Singleton(HTTP="Hyper Text Transfer Protocol")
+
+print(x)
+
+y = Singleton(SMTP='Simple Mail Transfer Protocol')
+
+print(y)
